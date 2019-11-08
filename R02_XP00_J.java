333c45e96abf35a42401301b5258873a8eddd556
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:16:19 -0500

    This is a complaint working solution

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,12 +1,44 @@
-// This is the non complaint solution
+
+import java.io.File;
 
 
+/******************************************************************************
+ *  Compilation:  javac R02_XP00_J.java
+ *  Execution:    java R02_XP00_J
+ *  Delete check
+ ******************************************************************************/
 
+/**
+ *
+ * @author sonut
+ */
+public class R02_XP00_J {
 
-
-
-public void delete File ( ) {
-File someFile = new File (”someFileName.txt”) ;
-// Do something with someFile
-someFile.delete();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+    deleteFile();
+  }
+    
+    
+    public static void deleteFile(){
+ 
+  File someFile = new File("someFileName.txt");
+  
+          
+  if (!someFile.delete()) {
+      
+       System.out.println("Delete UnSuccessful");
+  }
+  else
+     System.out.println("Delete Successful");
+      
+ 
 }
+
+ }
+
+
+// Output : Delete UnSuccessful
