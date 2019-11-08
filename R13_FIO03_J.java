fa299ad84c5f0febead6a9019cf6195fcbd8d7c2
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:20:51 -0500

    This is a complaint working solution

--- R13_FIO03_J.java
+++ R13_FIO03_J.java
@@ -1,8 +1,24 @@
-// This is a non complaint solution
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
-class TempFile {
-  public static void main(String[] args) throws IOException{
+/******************************************************************************
+ *  Compilation:  javac R13_FIO03_J.java
+ *  Execution:    java R13_FIO03_J
+ *  Creates file and shows error if already created
+ ******************************************************************************/
+
+/**
+ *
+ * @author sonut
+ */
+
+  public class R13_FIO03_J {
+      
+
+    public static void main(String[] args) throws IOException{
     File f = new File("tempnam.tmp");
     if (f.exists()) {
       System.out.println("This file already exists");
@@ -24,4 +40,4 @@ class TempFile {
       }
     }
   }
-}
+  }
