1da6c30f69df149bdf9659fbeede6481e0feb69b
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:17:13 -0500

    This is a complaint working solution

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,6 +1,38 @@
-// This is the non complaint solution
+import java.io.DataInputStream;
+import java.io.IOException;
 
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+
+
+/******************************************************************************
+ *  Compilation:  javac R03_NUM03_J.java
+ *  Execution:    java R03_NUM03_J\
+ *  Masking the integer with 32 One bits
+ ******************************************************************************/
+
+
+/**
+ *
+ * @author sonut
+ */
+public class R03_NUM03_J {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+  
+    DataInputStream dis = new DataInputStream(System.in);
+
+   long digit;
+    digit = getInteger(dis);
+    
+   System.out.println(digit);
+    
+        
+  }
+    
+  public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
 
+ }
