0203300b733b3b5471e258f4d3699cda0ae7f055
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:17:48 -0500

    This is a complaint working solution

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,7 +1,44 @@
-// This is the non complaint solution
 
-BigInteger x = new BigInteger("530500452766");
-byte[] byteArray = x.toByteArray();
-String s = new String(byteArray);
-byteArray = s.getBytes();
-x = new BigInteger(byteArray);
+
+import java.math.BigInteger;
+
+
+
+/******************************************************************************
+ *  Compilation:  javac R03_NUM03_J.java
+ *  Execution:    java R03_NUM03_J
+ * String rule for BigInteger
+ ******************************************************************************/
+
+
+
+
+/**
+ *
+ * @author sonut
+ */
+public class R04_STR03_J {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+    
+        BigInteger x = new BigInteger("530500452766");
+        String s = x.toString();  // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray); 
+        x = new BigInteger(ns);
+        System.out.println(x);
+        
+        
+  }
+    
+    
+    
+ 
+
+ }
+
+
