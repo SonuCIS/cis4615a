b8b6a7c10c11cb9a3a67025472fae0b9d2933d54
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:20:12 -0500

    This is a complaint working solution

--- R07_ERR01_J.java
+++ R07_ERR01_J.java
@@ -1,14 +1,51 @@
-// This is a non complaint solution
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
-class SecurityIOException extends IOException {/* ... */};
+/******************************************************************************
+ *  Compilation:  javac R07_ERR01_J.java
+ *  Execution:    java R07_ERR01_J filename
+ *  Exception Error
+ ******************************************************************************/
+
+
+
+
+/**
+ *
+ * @author sonut
+ */
+
+  public class R07_ERR01_J {
+      
+
+    public static void main(String[] args) {
+        
+     File file = null;
+    try {
+      file = new File(System.getenv("APPDATA") +
+             args[0]).getCanonicalFile();
+      if (!file.getPath().startsWith("c:\\homepath")) {
+        System.out.println("Invalid file");
+        return;
+      }
+    } catch (IOException x) {
+      System.out.println("Invalid file");
+      return;
+    }
  
-try {
-  FileInputStream fis =
-      new FileInputStream(System.getenv("APPDATA") + args[0]);
-} catch (FileNotFoundException e) {
-  // Log the exception
-  throw new SecurityIOException();
-}
-
-
+    try {
+      FileInputStream fis = new FileInputStream(file);
+    } catch (FileNotFoundException x) {
+      System.out.println("Invalid file");
+      return;
+    }
+    
+      
+      
+      
+    }
+  }
