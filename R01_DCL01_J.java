068058864c56fa03787f97ebabbe718ffe3bf53f
SonuCIS <57507181+SonuCIS@users.noreply.github.com>
2019-11-08 01:15:39 -0500

    This is a complaint working solution

--- R01_DCL01_J.java
+++ R01_DCL01_J.java
@@ -1,25 +1,37 @@
-/// This is a non complaint solution
 
+/******************************************************************************
+ *  Compilation:  javac R01_DCL01_J.java
+ *  Execution:    java R01_DCL01_J
+ *  
+ ******************************************************************************/
 
-class Vector {
-    private int val = 1;
-   
-    public boolean isEmpty() {
-      if (val == 1) {   // Compares with 1 instead of 0
-        return true;
-      } else {
-        return false;
-      }
-    }
-    // Other functionality is same as java.util.Vector
-  }
-   
-  // import java.util.Vector; omitted
-  public class VectorUser {
-    public static void main(String[] args) {
-      Vector v = new Vector();
-      if (v.isEmpty()) {
-        System.out.println("Vector is empty");
-      }
+/**
+ *
+ * @author sonut
+ */
+
+class MyVector {
+  private int val = 1;
+ 
+  public boolean isEmpty() {
+    if (val == 1) {   // Compares with 1 instead of 0
+      return true;
+    } else {
+      return false;
     }
   }
+  // Other functionality is same as java.util.Vector
+}
+ 
+// import java.util.Vector; omitted
+public class R01_DCL01_J {
+  public static void main(String[] args) {
+    MyVector v = new MyVector();
+    if (v.isEmpty()) {
+      System.out.println("Vector is empty");
+    }
+  }
+}
+
+
+// Vector is empty
